de28fb7f650f9b5cd91fc16b53174adae940a90e
NivDorfman <nivdorf@gmail.com>
2024-12-09 13:24:20 +0200

    Update Scrabble.java

--- Scrabble.java
+++ Scrabble.java
@@ -135,27 +135,27 @@ public class Scrabble {
     // 1. The letters in the word are removed from the hand, which becomes smaller.
     // 2. The user gets the Scrabble points of the entered word.
     // 3. The user is prompted to enter another word, or '.' to end the hand. 
-	public static void playHand(String hand) {
+public static void playHand(String hand) {
 		int n = hand.length();
-		int score = 0;  
+		int score = 0;
+		// Declares the variable in to refer to an object of type In, and initializes it
+		// to represent
+		// the stream of characters coming from the keyboard. Used for reading the
+		// user's inputs.
 		In in = new In();
 		while (hand.length() > 0) {
 			System.out.println("Current Hand: " + MyString.spacedString(hand));
 			System.out.println("Enter a word, or '.' to finish playing this hand:");
+			// Reads the next "token" from the keyboard. A token is defined as a string of
+			// non-whitespace characters. Whitespace is either space characters, or
+			// end-of-line characters.
 			String input = in.readString();
-
-			if(input.equals("."))
-			{
+			//// Replace the following break statement with code
+			//// that completes the hand playing loop
+			if (input.equals(".")) {
 				break;
 			}
-			score += wordScore(input);
-			hand = remove(input, hand);
-			if(hand.length() == 0)
-			{
-				break;
-			}
-		}
-		if (!MyString.subsetOf(input, hand)) {
+			if (!MyString.subsetOf(input, hand)) {
 				System.out.println("Invalid word. Try again.");
 			} else {
 				if (!isWordInDictionary(input)) {
@@ -170,13 +170,13 @@ public class Scrabble {
 					}
 				}
 			}
-		
+		}
 		if (hand.length() == 0) {
 			System.out.println("Ran out of letters. Total score: " + score + " points");
 		} else {
 			System.out.println("End of hand. Total score: " + score + " points");
 		}
-}	// Returns a string consisting of the string str1, minus all the characters in the
+	}// Returns a string consisting of the string str1, minus all the characters in the
     // string str2. Assumes (without checking) that str2 is a subset of str1.
 	public static String remove(String str1, String str2) {
         String result = str2;
